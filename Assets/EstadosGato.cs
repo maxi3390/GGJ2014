aabba7c6f25a704edeab10ac073cfca65163cd36
David Maximiliano Sosa <maxi3390@gmail.com>
2014-01-25 10:09:46 -0300

    Merge del laberinto y el gato. Viendo la colision entre ellos

--- Assets/EstadosGato.cs
+++ Assets/EstadosGato.cs
@@ -3,9 +3,8 @@ using System.Collections;
 
 public class EstadosGato : MonoBehaviour {
 
-	public bool mueveDerecha = false;
-	public bool moviendo = false;
-	//public bool mueveDerecha = false;
+	//private bool mueveDerecha = false;
+	//private bool moviendo = false;
 
 	// Use this for initialization
 	void Start () {
---
+++ Assets/Rotation.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotation : MonoBehaviour {
+	private Quaternion quat_dest;
+	private float orientation_z = 0.0f;
+	// Use this for initialization
+	void Start () {
+		quat_dest = transform.rotation;
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		this.transform.rotation = Quaternion.Slerp (this.transform.rotation, quat_dest, 0.1f);
+
+		//this.transform.Rotate (Vector3.forward,(int) this.transform.rotation.z + (Time.deltaTime * 30.0f), Space.World);
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			orientation_z += 90.0f;
+			if (Mathf.Abs(orientation_z - 360.0f) < Mathf.Epsilon)
+				orientation_z = 0.0f;
+			quat_dest = Quaternion.AngleAxis(orientation_z, Vector3.forward);
+		}
+	}
+
+}
