b86274c6c86140181b7046e8b785fa4fa1bb7dee
David Maximiliano Sosa <maxi3390@gmail.com>
2014-01-25 03:08:40 -0300

    Agregada maquinas de estado del gato

---
+++ Assets/EstadosGato.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EstadosGato : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.LeftArrow) || Input.GetKey (KeyCode.RightArrow)) {
+						this.GetComponent<Animator> ().SetBool ("isSaltando", false);
+						this.GetComponent<Animator> ().SetBool ("isCaminando", true);
+				} else {
+						if (Input.GetKey (KeyCode.Space)) {
+								this.GetComponent<Animator> ().SetBool ("isCaminando", false);
+								this.GetComponent<Animator> ().SetBool ("isSaltando", true);
+						} else {
+								this.GetComponent<Animator> ().SetBool ("isCaminando", false);
+								this.GetComponent<Animator> ().SetBool ("isSaltando", false);
+						}
+				}
+
+	}
+}
