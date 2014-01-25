2a6bb44a102e1f7d758efd775d1df199577e1222
Ciruelita <ceci.cerasifera@gmail.com>
2014-01-25 18:10:14 -0300

    Agregada la poción y cambios en el laberinto

---
+++ Assets/Timer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+
+
+	private float myTimer = 5.0f;
+
+	void Update () {
+
+				if (myTimer > 0) {
+						myTimer -= Time.deltaTime;
+				}
+				if (myTimer <= 0) {
+						Debug.Log ("GAME OVER");
+				}
+		}
+}
