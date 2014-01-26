16b0df76c10ec62bacb72eb6f4bd2a7cec9a7a99
Anacabe, Martin <prbinter@gmail.com>
2014-01-26 06:20:48 -0300

    error de codigo

--- Assets/distanciaSalida.cs
+++ Assets/distanciaSalida.cs
@@ -30,7 +30,7 @@ public class distanciaSalida : MonoBehaviour {
 		float intensidad = (1 / (distancia));
 		if (intensidad > 1)
 			luz.intensity = 1;
-		elserr
+		else
 			luz.intensity = intensidad;
 
 		if (Input.GetKeyDown (KeyCode.E)) {
