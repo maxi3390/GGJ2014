c26e4d93dd56d6e188ac4528b287d07c2553770c
Anacabe, Martin <prbinter@gmail.com>
2014-01-26 16:05:21 -0300

    Agregado el video

---
+++ Assets/video.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class video : MonoBehaviour {
+
+	private MovieTexture text;
+	// Use this for initialization
+	void Start () {
+		text = renderer.material.mainTexture as MovieTexture;
+		text.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0) || Input.anyKeyDown) {
+			text.Stop ();
+			Application.LoadLevel ("menu");
+		}
+	}
+}
