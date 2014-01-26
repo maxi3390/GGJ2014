bec92241a287a543272f4f42ef515877be5a2fa0
David Maximiliano Sosa <maxi3390@gmail.com>
2014-01-26 13:02:05 -0300

    El juego termina! :D

--- Assets/distanciaSalida.cs
+++ Assets/distanciaSalida.cs
@@ -5,24 +5,23 @@ using System.Collections;
 public class distanciaSalida : MonoBehaviour {
 	public Transform gato;							// traemos el gato
 	public Light luz;								// traemos la luz
-	public float minimaDistancia = float.MaxValue;
+	public float minimaDistancia = float.MinValue;
 	public Camera camara;							// traemos la camara
 	private TiltShiftHdr efecto;
 
 	// Use this for initialization
 	void Start () {
 		efecto = camara.GetComponent <TiltShiftHdr> ()/* as TiltShiftHdr*/;
-		//efecto.enabled = false;
+		efecto.enabled = true;
 	}
 
 	// Update is called once per frame
 	void Update () {
 
 		// This script calculates where the caraj is the cat
-		//float distancia = Mathf.Sqrt(pow(gato.position.x-
 		float distancia = Vector2.Distance (gato.position, this.transform.position);
 		if (distancia < minimaDistancia) {
-						minimaDistancia = distancia;
+			minimaDistancia = distancia;
 		}
 
 		// ahora seteamos la luminosidad en funcion de la distancia
@@ -39,7 +38,11 @@ public class distanciaSalida : MonoBehaviour {
 		efecto.blurArea=distancia*3;
 		efecto.maxBlurSize = 6;
 
-
 		//print (luz.intensity);
+	}
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		if (collision.gameObject.CompareTag ("Player"))
+				Application.LoadLevel ("winGame");
 	}
 }
